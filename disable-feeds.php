bfb8a5f7426cd8395955b65eede777d7c2994d06
Jesse Nickles <jesse@littlebizzy.com>
2024-09-20 17:43:50 +0700

    Update disable-feeds.php

--- disable-feeds.php
+++ disable-feeds.php
@@ -36,31 +36,27 @@ function disable_all_feeds() {
         exit;
     }
 }
-add_action( 'template_redirect', 'disable_all_feeds', 1 );
-
+add_action( 'template_redirect', 'disable_all_feeds', 20 );
 
 // Disable built-in feed types
 function disable_default_feeds() {
-    remove_action( 'do_feed_rdf', 'do_feed_rdf', 10 );
-    remove_action( 'do_feed_rss', 'do_feed_rss', 10 );
-    remove_action( 'do_feed_rss2', 'do_feed_rss2', 10 );
-    remove_action( 'do_feed_atom', 'do_feed_atom', 10 );
-    remove_action( 'do_feed_rss2_comments', 'do_feed_rss2_comments', 10 );
-    remove_action( 'do_feed_atom_comments', 'do_feed_atom_comments', 10 );
+    remove_action( 'do_feed_rdf', 'do_feed_rdf', 20 );
+    remove_action( 'do_feed_rss', 'do_feed_rss', 20 );
+    remove_action( 'do_feed_rss2', 'do_feed_rss2', 20 );
+    remove_action( 'do_feed_atom', 'do_feed_atom', 20 );
+    remove_action( 'do_feed_rss2_comments', 'do_feed_rss2_comments', 20 );
+    remove_action( 'do_feed_atom_comments', 'do_feed_atom_comments', 20 );
 }
-add_action( 'init', 'disable_default_feeds' );
+add_action( 'init', 'disable_default_feeds', 20 );
 
 // Disable feeds for custom post types
 function disable_custom_post_type_feeds() {
     // Get all public custom post types and disable their feeds
     foreach ( get_post_types( array( 'public' => true ) ) as $post_type ) {
-        remove_action( "do_feed_{$post_type}", 'do_feed_rss2', 10 );
+        remove_action( "do_feed_{$post_type}", 'do_feed_rss2', 20 );
     }
 }
-add_action( 'init', 'disable_custom_post_type_feeds' );
-
-
-
+add_action( 'init', 'disable_custom_post_type_feeds', 20 );
 
 // Remove feed links from <head>
 remove_action( 'wp_head', 'feed_links', 2 );
